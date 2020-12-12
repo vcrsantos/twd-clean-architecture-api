82c77cddc1d5c3dcf5ae26bf0bae6f95155f2bfc
Victor Santos <vcr.santoss@gmail.com>
2020-12-12 16:26:16 -0300

    test: validate emails with domain parts larger than 63

--- src/entitles/email-validation.spec.ts
+++ src/entitles/email-validation.spec.ts
@@ -41,6 +41,11 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeFalsy()
   })
 
+  test('should not accept domain with a part larger than 63 chars', () => {
+    const email = 'any@' + 'd'.repeat(64) + '.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
   test('should not accept local part with invalid char', () => {
     const email = 'any email@mail.com'
     expect(Email.validate(email)).toBeFalsy()
