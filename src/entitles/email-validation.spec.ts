06ffb506f17d69cff1296b6248dec30e04441590
Victor Santos <vcr.santoss@gmail.com>
2020-12-12 15:32:07 -0300

    test: validate local part larger than 64 chars

---
+++ src/entitles/email-validation.spec.ts
@@ -0,0 +1,23 @@
+import { Email } from './email'
+
+describe('Email validation', () => {
+  test('should not accept null string', () => {
+    const email = null
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept empty string', () => {
+    const email = ''
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should accept valid email', () => {
+    const email = 'any@mail.com'
+    expect(Email.validate(email)).toBeTruthy()
+  })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(65) + '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+})
