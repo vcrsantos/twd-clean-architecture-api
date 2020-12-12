010f9b959241d3784ec141927c04e1f4e043d95f
Victor Santos <vcr.santoss@gmail.com>
2020-12-12 16:23:52 -0300

    test: validate addiotional invalidemails

--- src/entitles/email-validation.spec.ts
+++ src/entitles/email-validation.spec.ts
@@ -45,4 +45,19 @@ describe('Email validation', () => {
     const email = 'any email@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept local part with two dots', () => {
+    const email = 'any..email@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept local part with ending dots', () => {
+    const email = 'anyemail.@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept email without an at-sign', () => {
+    const email = 'anymail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
