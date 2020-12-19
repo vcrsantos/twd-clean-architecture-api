d3cd9f2c6796a8058a76d5ff2542c404a312e807
Victor Santos <vcr.santoss@gmail.com>
2020-12-19 17:54:35 -0300

    feat: create valid user

--- src/entitles/user.ts
+++ src/entitles/user.ts
@@ -1,11 +1,19 @@
 import { UserData } from './user-data'
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { InvalidNameError } from './errors/invalid-name-error'
 import { Email } from './email'
 import { Name } from './name'
 
 export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
   static create (userData: UserData): Either <InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
 
@@ -18,5 +26,10 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
