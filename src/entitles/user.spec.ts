a42c9954b56bc44bbde02672f5b0befeb468a793
Victor Santos <vcr.santoss@gmail.com>
2020-12-19 17:42:23 -0300

    test: verify name while creating user

--- src/entitles/user.spec.ts
+++ src/entitles/user.spec.ts
@@ -1,11 +1,18 @@
 import { left } from '../shared/either'
 import { User } from './user'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 
 describe('User domain class', () => {
   test('should not create user with invalid e-mail address', () => {
     const invalidEmail = 'invalid_email'
     const error = User.create({ name: 'any_name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not create user with invalid name (too many characters)', () => {
+    const invalidName = 'O'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
