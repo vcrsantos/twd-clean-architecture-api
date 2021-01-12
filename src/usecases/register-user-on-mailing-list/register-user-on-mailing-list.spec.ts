4e939c78940e4f2d44119694187e10a467317dc3
Victor Santos <vcr.santoss@gmail.com>
2021-01-12 11:20:12 -0300

    test: prevent to add user with invalid email

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,6 @@
+import { InvalidEmailError } from '../../entitles/errors/invalid-email-error'
 import { UserData } from '../../entitles/user-data'
+import { left } from '../../shared/either'
 import { UserRepository } from './ports/user-repository'
 import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
 import { InMemoryUserRepository } from './repository/in-memory-user-repository'
@@ -6,7 +8,6 @@ import { InMemoryUserRepository } from './repository/in-memory-user-repository'
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
-    console.log(users)
     const repo: UserRepository = new InMemoryUserRepository(users)
     const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const name = 'any_name'
@@ -17,15 +18,15 @@ describe('Register user on mailing list use case', () => {
     expect(response.value.name).toBe('any_name')
   })
 
-  test('should not add user with invalid email', async () => {
+  test('should not add user with invalid email to mailing list', async () => {
     const users: UserData[] = []
     const repo: UserRepository = new InMemoryUserRepository(users)
     const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const name = 'any_name'
-    const email = 'any@email.com'
-    const response = await usecase.registerUserOnMailingList({ name, email })
-    const user = repo.findUserByEmail('any@email.com')
-    expect((await user).name).toBe('any_name')
-    expect(response.value.name).toBe('any_name')
+    const invalidemail = 'invalid_email'
+    const response = await usecase.registerUserOnMailingList({ name: name, email: invalidemail })
+    const user = await repo.findUserByEmail('any@email.com')
+    expect(user).toBeNull()
+    expect(response).toEqual(left(new InvalidEmailError()))
   })
 })
